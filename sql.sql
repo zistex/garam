211c4424622d29bd0e2c7b76e4eae9aeb52ede46
zistex <47314049+zistex@users.noreply.github.com>
2021-10-13 12:38:32 +0700

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 13, 2021 at 04:19 AM
+-- Generation Time: Oct 13, 2021 at 05:17 AM
 -- Server version: 8.0.21
 -- PHP Version: 7.4.9
 
@@ -34,6 +34,7 @@ CREATE TABLE IF NOT EXISTS `tbl_aset` (
   `idpeta` int NOT NULL,
   `nama` varchar(255) NOT NULL,
   `gambar` varchar(250) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -41,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `tbl_aset` (
 -- Dumping data for table `tbl_aset`
 --
 
-INSERT INTO `tbl_aset` (`id`, `kategori`, `idpeta`, `nama`, `gambar`) VALUES
-(1, 1, 1, 'ladang 1', 'gambar1.png');
+INSERT INTO `tbl_aset` (`id`, `kategori`, `idpeta`, `nama`, `gambar`, `alamat`) VALUES
+(1, 1, 1, 'ladang 1', 'ladang1.jpeg', 'jln. abc');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
